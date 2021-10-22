3ad1a24a863e3bb8823fd1aec940237dc9e54572
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-10-22 11:30:47 +0200

    setup

--- GildedRose.Tests/TestAssemblyTests.cs
+++ GildedRose.Tests/TestAssemblyTests.cs
@@ -1,4 +1,6 @@
 ﻿using Xunit;
+using GildedRose;
+using System.IO;
 
 namespace GildedRose.Tests
 {
@@ -8,6 +10,35 @@ namespace GildedRose.Tests
         public void TestTheTruth()
         {
             Assert.True(true);
+        }
+        [Fact]
+        public void Test_OpeningPrintStatement()
+        {
+        //Given
+        var writer = new StringWriter();
+        //var reader = new StringReader("1");
+
+        System.Console.SetOut(writer);
+        //System.Console.SetIn(reader);
+
+        Program.Main(new string[0]);
+        string actual = writer.GetStringBuilder().ToString().Trim();
+        //When
+        //Then
+        Assert.Equal("OMGHAI!", actual);
+        }
+
+        [Fact]
+        public void UpdateQuality_UpdateOnRagnoros_MakesNoChange()
+        {
+        //Given
+        var app = new Program();
+        var expected = new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80};
+        //When
+        for (int i = 0; i < 10; i++){app.UpdateQuality();}
+        Item handORagno = app.Items[3];
+        //Then
+        Assert.Equal(expected, handORagno);
         }
     }
 }
